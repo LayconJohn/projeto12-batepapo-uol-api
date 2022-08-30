6cdf1b8b63dc4bad02f9370c48f101b1ba18d40d
layconjohn <layconjohn@gmail.com>
2022-08-30 10:16:19 -0300

    feat: Rota /participantes get e post (sem o uso do MongoDB e Joi)

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+import chalk from "chalk";
+import dayjs from "dayjs";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const participantes = [];
+const mensagens = [];
+
+app.post("/participants", (req, res) => {
+    const {name} = req.body;
+
+    if (!name) {
+        res.sendStatus(422);
+        return;
+    }
+
+    const nameJaExiste = participantes.find(value => value.name === name);
+    if (nameJaExiste) {
+        res.sendStatus(409);
+        return;
+    }
+
+    participantes.push({name: name, lastStatus: Date.now()});
+
+    mensagens.push({from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format("HH:mm:mm")})
+
+    //console.log(participantes);
+    //console.log(mensagens);
+    res.sendStatus(201);
+})
+
+app.get("/participants", (req, res) => {
+    res.status(200).send(participantes);
+})
+
+app.listen(5000, () => {
+    console.log(chalk.greenBright("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"))
+    console.log(chalk.green("Servidor rodando na porta 5000"))
+    console.log(chalk.greenBright("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"))
+})
