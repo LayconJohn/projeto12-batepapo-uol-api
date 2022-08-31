449f4993721b7d52e3255020130726ace9755bdb
layconjohn <layconjohn@gmail.com>
2022-08-31 10:57:08 -0300

    fix: Inserção do mongoDB na rota participantes

--- src/index.js
+++ src/index.js
@@ -3,22 +3,43 @@ import cors from "cors";
 import chalk from "chalk";
 import dayjs from "dayjs";
 import Joi from "joi";
+import { MongoClient } from "mongodb";
 
 const app = express();
+const url = "mongodb://localhost:27017";
+const cliente = new MongoClient(url);
+let db;
+
+cliente.connect().then(() => {
+	db = cliente.db("bate_papo_uol");
+});
+
+let participantes;
+let mensagens;
+
 
 app.use(cors());
 app.use(express.json());
 
-const participantes = [];
-const mensagens = [];
 
 app.post("/participants", (req, res) => {
+
     const {name} = req.body;
 
+    //validation
     const schema = Joi.object({
         name: Joi.string().min(1).required()
     })
 
+    //verificar esse teste
+    db.collection("participantes").find().toArray().then(users => {
+        //console.log(users)
+        const nameJaExiste = users.find(value => value.name === name);
+        if (nameJaExiste) {
+            res.sendStatus(409);
+            return;
+        }
+    });
 
     const nameContemErro = schema.validate({name}).error
     if (nameContemErro) {
@@ -26,24 +47,30 @@ app.post("/participants", (req, res) => {
         return;
     }
 
-    const nameJaExiste = participantes.find(value => value.name === name);
-    if (nameJaExiste) {
-        res.sendStatus(409);
-        return;
-    }
+    db.collection("participantes").insertOne({name: name, lastStatus: Date.now()});
+    db.collection("mensagens").insertOne({from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format("HH:mm:mm")})
 
-    participantes.push({name: name, lastStatus: Date.now()});
-
-    mensagens.push({from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format("HH:mm:mm")})
-
-    //console.log(participantes);
-    //console.log(mensagens);
     res.sendStatus(201);
 })
 
 app.get("/participants", (req, res) => {
-    res.status(200).send(participantes);
+    
+    db.collection("participantes").find().toArray().then(users => {
+        res.status(200).send({participants: users});
+    });
+    
 })
+
+app.post("/messages", (req, res) => {
+    const {to, text, type} = req.body;
+    const {from} = req.headers.user;
+
+    const body = {from: from, to: to, text: text, type: type, time: dayjs().format("HH:mm:mm")}
+    console.log(body)
+
+    res.sendStatus(201);
+})
+
 
 app.listen(5000, () => {
     console.log(chalk.greenBright("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"))
